bd28e98aea63077deec9bc33aac2acc95932867f
TylerWon <won.tyler1@gmail.com>
2023-05-24 22:44:10 -0700

    setup database integration for server

---
+++ server/db.js
@@ -0,0 +1,13 @@
+const pgp = require('pg-promise')();
+
+const cn = {
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+};
+
+const db = pgp(cn);
+
+module.exports = db;
