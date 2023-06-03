4539e30ec8cb83ccec322b121690571b2a5b2c43
TylerWon <won.tyler1@gmail.com>
2023-06-02 20:43:04 -0700

    add more specific error handling for auth routes

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -9,22 +9,24 @@ const queries = require("../utils/queries");
 // Passport strategy for authentication with username and password
 passport.use(
     new LocalStrategy(async (username, password, cb) => {
-        try {
-            const user = await db.oneOrNone(queries.users.getUser, [username]);
+        let hashedPassword;
 
-            if (!user) {
-                return cb(null, false); // authentication failure - username incorrect
-            }
-
-            const hashedPassword = crypto.pbkdf2Sync(password, user.salt, 1024, 32, "sha256");
-            if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
-                return cb(null, false); // authentication failure - password incorrect
-            }
-
-            return cb(null, user); // authentication success
-        } catch (err) {
-            return cb(err); // > 1 user retrieved from db or error when computing hashedPassword with pbkdf2
+        const user = await db.oneOrNone(queries.users.getUser, [username]);
+        if (!user) {
+            return cb(null, false); // authentication failure - username incorrect
         }
+
+        try {
+            hashedPassword = crypto.pbkdf2Sync(password, user.salt, 1024, 32, "sha256");
+        } catch (err) {
+            return cb(err); // error when computing hashed password
+        }
+
+        if (!crypto.timingSafeEqual(user.password, hashedPassword)) {
+            return cb(null, false); // authentication failure - password incorrect
+        }
+
+        return cb(null, user); // authentication success
     })
 );
 
@@ -49,30 +51,37 @@ const logout = (req, res, next) => {
         if (err) {
             return next(err);
         }
+
         res.send("Logout successful");
     });
 };
 
 const signup = async (req, res, next) => {
+    let token, hashedPassword, user;
     const username = req.body.username;
     const password = req.body.password;
     const salt = crypto.randomBytes(16);
 
     try {
-        const hashedPassword = crypto.pbkdf2Sync(password, salt, 1024, 32, "sha256");
-        const token = jwt.sign({ username: username }, process.env.JWT_SECRET);
-        const user = await db.one(queries.users.createUser, [username, hashedPassword, salt, token]);
-
-        req.login(user, (err) => {
-            if (err) {
-                return next(err);
-            }
-
-            res.send("Sign up successful");
-        });
+        hashedPassword = crypto.pbkdf2Sync(password, salt, 1024, 32, "sha256");
+        token = jwt.sign({ username: username }, process.env.JWT_SECRET);
     } catch (err) {
-        next(err);
+        next(err); // error when computing hashed password or token
     }
+
+    try {
+        user = await db.one(queries.users.createUser, [username, hashedPassword, salt, token]);
+    } catch (err) {
+        res.status(400).send("Username already exists");
+    }
+
+    req.login(user, (err) => {
+        if (err) {
+            return next(err);
+        }
+
+        res.send("Sign up successful");
+    });
 };
 
 module.exports = {
