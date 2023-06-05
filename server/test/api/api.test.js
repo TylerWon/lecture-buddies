ad0c814317d11f22adf840e082e67a7c4513ad90
TylerWon <won.tyler1@gmail.com>
2023-06-04 23:18:47 -0700

    add tests for subject routes

--- server/test/api/api.test.js
+++ server/test/api/api.test.js
@@ -4,46 +4,79 @@ const app = require("../../src/app");
 const db = require("../../src/configs/db.config");
 const queries = require("../utils/queries");
 
-describe("school routes tests", () => {
+describe("api tests", () => {
     let school;
     let subject;
+    let course;
 
     beforeAll(async () => {
         school = await db.one(queries.schools.createSchool, ["University of British Columbia", "www.ubc.ca/logo.png"]);
         subject = await db.one(queries.subjects.createSubject, [school.school_id, "CPSC"]);
+        course = await db.one(queries.courses.createCourse, [
+            subject.subject_id,
+            "110",
+            "Computation, Programs, and Programming",
+        ]);
     });
 
     afterAll(async () => {
         await db.none(queries.schools.deleteSchool, [school.school_id]);
         await db.none(queries.subjects.deleteSubject, [subject.subject_id]);
+        await db.none(queries.courses.deleteCourse, [course.course_id]);
         db.$pool.end();
     });
 
-    describe("/schools", () => {
-        test("GET - should return all schools", async () => {
-            const response = await request(app).get("/schools");
-            expect(response.statusCode).toEqual(200);
-            expect(response.body).toEqual([
-                {
-                    school_id: school.school_id,
-                    school_name: school.school_name,
-                    logo_url: school.logo_url,
-                },
-            ]);
+    /**
+     * Checks that a GET request to an endpoint returns the expected status code and body
+     *
+     * @param {string} endpoint - the endpoint to send the GET request to
+     * @param {number} expectedStatusCode - the expected status code of the response
+     * @param {any} expectedBody - the expected body of the response
+     */
+    async function verifyGetRequestResponse(endpoint, expectedStatusCode, expectedBody) {
+        const response = await request(app).get(endpoint);
+        expect(response.statusCode).toEqual(expectedStatusCode);
+        expect(response.body).toEqual(expectedBody);
+    }
+
+    describe("school routes tests", () => {
+        describe("/schools", () => {
+            test("GET - should return all schools", async () => {
+                await verifyGetRequestResponse("/schools", 200, [
+                    {
+                        school_id: school.school_id,
+                        school_name: school.school_name,
+                        logo_url: school.logo_url,
+                    },
+                ]);
+            });
+        });
+
+        describe("/schools/{school_id}/subjects", () => {
+            test("GET - should return subjects for a school", async () => {
+                await verifyGetRequestResponse(`/schools/${school.school_id}/subjects`, 200, [
+                    {
+                        subject_id: subject.subject_id,
+                        school_id: subject.school_id,
+                        subject_name: subject.subject_name,
+                    },
+                ]);
+            });
         });
     });
 
-    describe("/schools/{school_id}/subjects", () => {
-        test("GET - should return subjects for a school", async () => {
-            const response = await request(app).get(`/schools/${school.school_id}/subjects`);
-            expect(response.statusCode).toEqual(200);
-            expect(response.body).toEqual([
-                {
-                    subject_id: subject.subject_id,
-                    school_id: subject.school_id,
-                    subject_name: subject.subject_name,
-                },
-            ]);
+    describe("subject routes tests", () => {
+        describe("/subjects/{subject_id}/courses", () => {
+            test("GET - should return courses for a subject", async () => {
+                await verifyGetRequestResponse(`/subjects/${subject.subject_id}/courses`, 200, [
+                    {
+                        course_id: course.course_id,
+                        subject_id: course.subject_id,
+                        course_number: course.course_number,
+                        course_name: course.course_name,
+                    },
+                ]);
+            });
         });
     });
 });
--- server/test/utils/queries.js
+++ server/test/utils/queries.js
@@ -7,6 +7,10 @@ const queries = {
         createSubject: "INSERT INTO subjects (school_id, subject_name) VALUES ($1, $2) RETURNING *",
         deleteSubject: "DELETE FROM subjects WHERE subject_id = $1",
     },
+    courses: {
+        createCourse: "INSERT INTO courses (subject_id, course_number, course_name) VALUES ($1, $2, $3) RETURNING *",
+        deleteCourse: "DELETE FROM courses WHERE course_id = $1",
+    },
 };
 
 module.exports = queries;
