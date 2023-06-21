a9e643be97ae2372e7dae1df26adaa917929a3ab
TylerWon <won.tyler1@gmail.com>
2023-06-20 21:00:18 -0700

    remove create user logic from db.config.js

--- server/src/configs/db.config.js
+++ server/src/configs/db.config.js
@@ -1,5 +1,4 @@
 const pgp = require("pg-promise")();
-const crypto = require("crypto");
 
 const cn = {
     host: process.env.DB_HOST,
@@ -10,13 +9,5 @@ const cn = {
 };
 
 const db = pgp(cn);
-
-// create an initial user
-// var salt = crypto.randomBytes(16);
-// db.one("INSERT INTO users (username, password, salt) VALUES ($1, $2, $3) RETURNING user_id", [
-//     "won.tyler1@gmail.com",
-//     crypto.pbkdf2Sync("password", salt, 1024, 32, "sha256"),
-//     salt,
-// ]);
 
 module.exports = db;
