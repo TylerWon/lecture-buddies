9aad753602d0c0067747811aadc3e101d7bb6220
TylerWon <won.tyler1@gmail.com>
2023-06-24 23:34:36 -0700

    add more tests for get messages for conversation route

--- server/test/api/conversation.test.js
+++ server/test/api/conversation.test.js
@@ -3,10 +3,13 @@ const request = require("supertest");
 const app = require("../../src/app");
 const db = require("../../src/configs/db.config");
 const {
+    cleanUpDatabase,
+    createConversation,
+    createConversationMember,
+    createMessage,
     createSchool,
     createStudent,
     createUser,
-    cleanUpDatabase,
     verifyGetRequestResponse,
     verifyPostRequestResponseWithAuth,
 } = require("../utils/helpers");
@@ -17,6 +20,9 @@ describe("conversation routes tests", () => {
     let school1;
     let student1;
     let student2;
+    let conversation1;
+    let message1;
+    let message2;
 
     const user1Username = "won.tyler1@gmail.com";
     const user1Password = "password1";
@@ -50,6 +56,21 @@ describe("conversation routes tests", () => {
             "Computer Science",
             "www.connorwon.com/profile_photo.jpg",
             "Hello. I'm Connor. I'm a 3rd year computer science student at UBC."
+        );
+        conversation1 = await createConversation(db, "DM");
+        await createConversationMember(db, conversation1.conversation_id, student1.student_id);
+        await createConversationMember(db, conversation1.conversation_id, student2.student_id);
+        message1 = await createMessage(
+            db,
+            conversation1.conversation_id,
+            student1.student_id,
+            "This is message 1 in conversation 1"
+        );
+        message2 = await createMessage(
+            db,
+            conversation1.conversation_id,
+            student2.student_id,
+            "This is message 2 in conversation 1"
         );
     });
 
@@ -121,20 +142,60 @@ describe("conversation routes tests", () => {
     });
 
     describe("/conversations/{conversation_id}/messages", () => {
-        test("GET - should return the messages for a conversation", async () => {
+        test("GET - should return the messages for a conversation (order by date)", async () => {
             await verifyGetRequestResponse(
                 app,
-                `/conversations/${conversation1.conversation_id}/messages`,
+                `/conversations/${conversation1.conversation_id}/messages?order_by=date&offset=0&limit=2`,
                 user1.token,
                 200,
-                [section1]
+                [message1, message2]
+            );
+        });
+
+        test("GET - should return the messages for a conversation (order by -date)", async () => {
+            await verifyGetRequestResponse(
+                app,
+                `/conversations/${conversation1.conversation_id}/messages?order_by=-date&offset=0&limit=2`,
+                user1.token,
+                200,
+                [message2, message1]
+            );
+        });
+
+        test("GET - should return the messages for a conversation (0 < offset < total number of results)", async () => {
+            await verifyGetRequestResponse(
+                app,
+                `/conversations/${conversation1.conversation_id}/messages?order_by=date&offset=1&limit=1`,
+                user1.token,
+                200,
+                [message2]
+            );
+        });
+
+        test("GET - should return the messages for a conversation (limit >= total number of results)", async () => {
+            await verifyGetRequestResponse(
+                app,
+                `/conversations/${conversation1.conversation_id}/messages?order_by=date&offset=0&limit=10`,
+                user1.token,
+                200,
+                [message1, message2]
+            );
+        });
+
+        test("GET - should return nothing when offset >= total number of results", async () => {
+            await verifyGetRequestResponse(
+                app,
+                `/conversations/${conversation1.conversation_id}/messages?order_by=date&offset=2&limit=2`,
+                user1.token,
+                200,
+                []
             );
         });
 
         test("GET - should return error message when the conversation_id path parameter does not correspond to a conversation", async () => {
             await verifyGetRequestResponse(
                 app,
-                `/conversations/${conversation1.conversation_id + 100}/messages`,
+                `/conversations/${conversation1.conversation_id + 100}/messages?order_by=date&offset=0&limit=2`,
                 user1.token,
                 400,
                 {
@@ -143,12 +204,122 @@ describe("conversation routes tests", () => {
             );
         });
 
-        test("GET - should return error message when request is unauthenticated", async () => {
-            await verifyPutRequestResponse(
+        test("GET - should return error message when missing the order_by query parameter", async () => {
+            await verifyGetRequestResponse(
                 app,
-                `/conversations/${conversation1.conversation_id}/messages`,
+                `/conversations/${conversation1.conversation_id}/messages?offset=0&limit=2`,
+                user1.token,
+                400,
+                [
+                    {
+                        type: "field",
+                        location: "query",
+                        path: "order_by",
+                        msg: "order_by is required",
+                    },
+                ]
+            );
+        });
+
+        test("GET - should return error message when missing the offset query parameter", async () => {
+            await verifyGetRequestResponse(
+                app,
+                `/conversations/${conversation1.conversation_id}/messages?order_by=date&limit=2`,
+                user1.token,
+                400,
+                [
+                    {
+                        type: "field",
+                        location: "query",
+                        path: "offset",
+                        msg: "offset is required",
+                    },
+                ]
+            );
+        });
+
+        test("GET - should return error message when missing the limit query parameter", async () => {
+            await verifyGetRequestResponse(
+                app,
+                `/conversations/${conversation1.conversation_id}/messages?order_by=date&offset=0`,
+                user1.token,
+                400,
+                [
+                    {
+                        type: "field",
+                        location: "query",
+                        path: "limit",
+                        msg: "limit is required",
+                    },
+                ]
+            );
+        });
+
+        test("GET - should return error message when the order_by query parameter value is not a valid option", async () => {
+            const orderBy = "conversation_name";
+
+            await verifyGetRequestResponse(
+                app,
+                `/conversations/${conversation1.conversation_id}/messages?order_by=${orderBy}&offset=0&limit=2`,
+                user1.token,
+                400,
+                [
+                    {
+                        type: "field",
+                        location: "query",
+                        path: "order_by",
+                        value: orderBy,
+                        msg: "order_by must be one of 'date' or '-date'",
+                    },
+                ]
+            );
+        });
+
+        test("GET - should return error message when the offset query parameter value is negative", async () => {
+            const offset = "-1";
+
+            await verifyGetRequestResponse(
+                app,
+                `/conversations/${conversation1.conversation_id}/messages?order_by=date&offset=${offset}&limit=2`,
+                user1.token,
+                400,
+                [
+                    {
+                        type: "field",
+                        location: "query",
+                        path: "offset",
+                        value: offset,
+                        msg: "offset must be a positive integer",
+                    },
+                ]
+            );
+        });
+
+        test("GET - should return error message when the limit query parameter value is negative", async () => {
+            const limit = "-1";
+
+            await verifyGetRequestResponse(
+                app,
+                `/conversations/${conversation1.conversation_id}/messages?order_by=date&offset=0&limit=${limit}`,
+                user1.token,
+                400,
+                [
+                    {
+                        type: "field",
+                        location: "query",
+                        path: "limit",
+                        value: limit,
+                        msg: "limit must be a positive integer",
+                    },
+                ]
+            );
+        });
+
+        test("GET - should return error message when request is unauthenticated", async () => {
+            await verifyGetRequestResponse(
+                app,
+                `/conversations/${conversation1.conversation_id}/messages?order_by=date&offset=0&limit=2`,
                 undefined,
-                payload,
                 401,
                 {
                     message: "unauthorized",
