a53f016a5829490a9bcc4975023cbfe5cde56bab
TylerWon <won.tyler1@gmail.com>
2023-07-06 21:39:47 -0700

    specify custom font sizes

--- client/src/assets/styles/theme.js
+++ client/src/assets/styles/theme.js
@@ -1,6 +1,6 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
-const theme = createTheme({
+let theme = createTheme({
     palette: {
         mode: "light",
         primary: {
@@ -18,7 +18,48 @@ const theme = createTheme({
     },
     typography: {
         fontFamily: ["Lato", "sans-serif"].join(","),
+        h1: {
+            fontSize: "48px",
+        },
+        h2: {
+            fontSize: "40px",
+        },
+        h3: {
+            fontSize: "36px",
+        },
+        h4: {
+            fontSize: "32px",
+        },
+        h5: {
+            fontSize: "24px",
+        },
+        h6: {
+            fontSize: "20px",
+        },
+        subtitle1: {
+            fontSize: "16px",
+        },
+        subtitle2: {
+            fontSize: "14px",
+        },
+        body1: {
+            fontSize: "16px",
+        },
+        body2: {
+            fontSize: "14px",
+        },
+        button: {
+            fontSize: "14px",
+        },
+        caption: {
+            fontSize: "12px",
+        },
+        overline: {
+            fontSize: "10px",
+        },
     },
 });
+
+theme = responsiveFontSizes(theme);
 
 export default theme;
