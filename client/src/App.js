6e2cf115d4353c9d85df84b0140850f50a546593
TylerWon <won.tyler1@gmail.com>
2023-07-06 20:14:24 -0700

    add Home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
-const App = () => {
-    return <></>;
-};
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<Home />} />));
+
+function App() {
+    return <RouterProvider router={router} />;
+}
 
 export default App;
---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>TODO</h1>;
+}
+
+export default Home;
