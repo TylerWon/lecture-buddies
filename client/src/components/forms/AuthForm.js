d955092b9406d9405d986e02e21d36faaf97b7c1
TylerWon <won.tyler1@gmail.com>
2023-07-14 11:41:33 -0700

    implement first step of sign up process

---
+++ client/src/components/forms/AuthForm.js
@@ -0,0 +1,57 @@
+import { Button, FormControl, IconButton, InputAdornment, Stack, TextField } from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useState } from "react";
+
+function AuthForm(props) {
+    const { formik, submitButtonText } = props;
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    // Handler for when show password button is clicked
+    const handleShowPasswordClick = () => {
+        setShowPassword(!showPassword);
+    };
+
+    return (
+        <FormControl component="form" fullWidth onSubmit={formik.handleSubmit}>
+            <Stack direction="column" alignItems="center" spacing={2}>
+                <TextField
+                    fullWidth
+                    id="email"
+                    name="email"
+                    label="Email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                />
+                <TextField
+                    fullWidth
+                    id="password"
+                    name="password"
+                    label="Password"
+                    type={showPassword ? "text" : "password"}
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    error={formik.touched.password && Boolean(formik.errors.password)}
+                    helperText={formik.touched.password && formik.errors.password}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton onClick={handleShowPasswordClick} edge="end">
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
+                <Button color="primary" variant="contained" fullWidth type="submit">
+                    {submitButtonText}
+                </Button>
+            </Stack>
+        </FormControl>
+    );
+}
+
+export default AuthForm;
--- client/src/pages/Home/signup/SignUpModal.js
+++ client/src/pages/Home/signup/SignUpModal.js
@@ -1,7 +1,90 @@
+import { Box, Container, Dialog, Typography, useTheme } from "@mui/material";
+import { useFormik } from "formik";
+import { useContext } from "react";
+import * as yup from "yup";
+import YupPassword from "yup-password";
+
+import { UserContext } from "../../../contexts/UserContext";
+
+import AuthForm from "../../../components/forms/AuthForm";
+
+YupPassword(yup);
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+const validationSchema = yup.object({
+    email: yup.string().email("Invalid email address").required("Email is required"),
+    password: yup
+        .string()
+        .password()
+        .min(8, "Password must be at least 8 characters long")
+        .minLowercase(1, "Password must contain at least 1 lowercase letter")
+        .minUppercase(1, "Password must contain at least 1 uppercase letter")
+        .minNumbers(1, "Password must contain at least 1 number")
+        .minSymbols(1, "Password must contain at least 1 symbol")
+        .required("Password is required"),
+});
+
 function SignUpModal(props) {
     const { showSignUpModal, setShowSignUpModal } = props;
 
-    return;
+    const { setUserId } = useContext(UserContext);
+    const theme = useTheme();
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            handleAuthFormSubmit(values);
+        },
+    });
+
+    // Handler for when sign up modal is closed
+    const handleSignUpModalClose = () => {
+        setShowSignUpModal(false);
+    };
+
+    // Handler for when AuthForm is submitted
+    const handleAuthFormSubmit = async (values) => {
+        try {
+            const response = await fetch(`${API_BASE_URL}/auth/signup`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username: values.email,
+                    password: values.password,
+                }),
+                credentials: "include",
+            });
+
+            if (response.status === 400) {
+                formik.setErrors({ email: "An account with this email already exists" });
+                return;
+            }
+
+            const data = await response.json();
+
+            setUserId(data.userId);
+        } catch (err) {
+            console.log(err); // unexpected error
+        }
+    };
+
+    return (
+        <Dialog open={showSignUpModal} onClose={handleSignUpModalClose} fullWidth maxWidth="sm">
+            <Box padding="20px" bgcolor={theme.palette.common.white}>
+                <Typography variant="h3">Sign up</Typography>
+                <Container maxWidth="xs" sx={{ marginTop: "35px" }}>
+                    <AuthForm formik={formik} submitButtonText="Sign up" />
+                </Container>
+            </Box>
+        </Dialog>
+    );
 }
 
 export default SignUpModal;
